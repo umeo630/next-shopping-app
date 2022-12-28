65ee917502589e488a6d20cd5c45a7814c173cf5
Hikaru-Umezawa <hikaru.19970630@gmail.com>
2022-12-28 22:32:05 +0900

    add: styled-components

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,34 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { DocumentContext, DocumentInitialProps } from "next/document";
+import React from "react";
+import { ServerStyleSheet } from "styled-components";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext,
+  ): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet()
+    const originRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+
+      return {
+        ...initialProps,
+        styles: [
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>,
+        ],
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
 }
