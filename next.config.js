768a02a208328566aeed0bc726a25f8a56976c46
Hikaru-Umezawa <hikaru.19970630@gmail.com>
2022-12-31 10:37:20 +0900

    add: テスト環境

--- next.config.js
+++ next.config.js
@@ -1,9 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  compiler: {
-    styledComponents: true,
-  }
+  compiler: (() => {
+    let compilerConfig = {
+      styledComponents: true,
+    }
+
+    if (process.env.NODE_ENV === 'production') {
+      compilerConfig = {
+        ...compilerConfig,
+        // 本番環境ではdata-testid属性を削除
+        reactRemoveProperties: {properties: ['^data-testid$']},
+      }
+    }
+
+    return compilerConfig
+  }) (),
 }
 
 module.exports = nextConfig
