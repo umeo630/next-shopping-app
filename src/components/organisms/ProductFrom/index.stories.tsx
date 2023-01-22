15b62e8be51c0246c971585543daf52becba73de
Hikaru-Umezawa <hikaru.19970630@gmail.com>
2023-01-22 18:22:12 +0900

    add: ProductForm

---
+++ src/components/organisms/ProductFrom/index.stories.tsx
@@ -0,0 +1,20 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+import ProductForm from '.'
+
+export default {
+  title: 'Organisms/ProductForm',
+  argTypes: {
+    onProductSave: {
+      description: '出品ボタンを押した時のイベントハンドラ',
+      table: {
+        type: { summary: 'function' },
+      },
+    },
+  },
+} as ComponentMeta<typeof ProductForm>
+
+const Template: ComponentStory<typeof ProductForm> = (args) => (
+  <ProductForm {...args} />
+)
+
+export const Form = Template.bind({})
